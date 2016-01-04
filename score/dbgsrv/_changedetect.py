43392833fd56f3cd177c3b7b12f39860d1881c53
Necdet Can Atesman <c.atesman@gmx.net>
2016-01-04 13:54:55 +0100

    Better change detection
    The built-in ChangeDetector no longer intercepts imports, but gathers
    imported modules periodically.

--- score/dbgsrv/_changedetect.py
+++ score/dbgsrv/_changedetect.py
@@ -1,8 +1,35 @@
+# Copyright © 2015 STRG.AT GmbH, Vienna, Austria
+#
+# This file is part of the The SCORE Framework.
+#
+# The SCORE Framework and all its parts are free software: you can redistribute
+# them and/or modify them under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation which is in the
+# file named COPYING.LESSER.txt.
+#
+# The SCORE Framework and all its parts are distributed without any WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. For more details see the GNU Lesser General Public
+# License.
+#
+# If you have not received a copy of the GNU Lesser General Public License see
+# http://www.gnu.org/licenses/.
+#
+# The License-Agreement realised between you as Licensee and STRG.AT GmbH as
+# Licenser including the issue of its valid conclusion and its pre- and
+# post-contractual effects is governed by the laws of Austria. Any disputes
+# concerning this License-Agreement including the issue of its valid conclusion
+# and its pre- and post-contractual effects are exclusively decided by the
+# competent court, in whose district STRG.AT GmbH has its registered seat, at
+# the discretion of STRG.AT GmbH also the competent court, in whose district the
+# Licensee has his registered seat, an establishment or assets.
+
 import pyinotify
 import os
-import builtins
 import sys
 import logging
+import threading
+import time
 
 log = logging.getLogger('score.dbgsrv')
 mask = pyinotify.IN_MODIFY | pyinotify.IN_CREATE | pyinotify.IN_DELETE | \
@@ -12,39 +39,54 @@ mask = pyinotify.IN_MODIFY | pyinotify.IN_CREATE | pyinotify.IN_DELETE | \
 class ChangeDetector:
 
     def __init__(self, *, autostart=True):
-        self.observed_files = set()
-        self.observed_dirs = set()
-        self.file2modules = {}
         self.callbacks = []
         self.wm = pyinotify.WatchManager()
         self.notifier = pyinotify.ThreadedNotifier(self.wm, self)
-        self._original_import = builtins.__import__
-        builtins.__import__ = self._import
-        for module in sys.modules.values():
-            self.observe_module(module)
+        self.gatherer = threading.Thread(target=self._gather_modules)
+        self.running = False
         if autostart:
             self.start()
 
+    def _gather_modules(self):
+        while self.running:
+            for module in list(sys.modules.values()):
+                self.observe_module(module)
+                if not self.running:
+                    return
+            time.sleep(0.5)
+
     def start(self):
+        self.observed_files = set()
+        self.observed_dirs = set()
+        self.file2modules = {}
+        self.running = True
+        self.gatherer.start()
         self.notifier.start()
 
     def stop(self):
+        if not self.running:
+            return
+        self.running = False
         self.notifier.stop()
         self.notifier.join()
-
-    def _import(self, *args, **kwargs):
-        module = self._original_import(*args, **kwargs)
-        self.observe_module(module)
-        return module
+        self.gatherer.join()
 
     def observe_module(self, module):
         try:
             file = module.__file__
         except AttributeError:
             return
-        else:
-            if file:
-                self.observe(file, module)
+        if not file:
+            return
+        old = None
+        while not os.path.isfile(file):
+            old = file
+            file = os.path.dirname(file)
+            if file == old:
+                return
+        if file[-4:] in ('.pyc', '.pyo'):
+            file = file[:-1]
+        self.observe(file, module)
 
     def observe(self, file, module=None):
         if file in self.observed_files:
@@ -62,7 +104,12 @@ class ChangeDetector:
         if dir in self.observed_dirs:
             return
         self.observed_dirs.add(dir)
-        self.wm.add_watch(dir, mask, proc_fun=self._changed)
+        if self.running:
+            # safeguarding against startup errors: self.wm.add_watch fails with
+            # errno EBADF (bad file descriptor) if the thread is not running.
+            # this happens most commonly when the Runner has a startup issue,
+            # when lots of new watches are added.
+            self.wm.add_watch(dir, mask, proc_fun=self._changed)
 
     def onchange(self, callback):
         self.callbacks.append(callback)
