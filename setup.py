ab7b6d33861c995f42fec46063cdb4e915c029f7
Necdet Can Atesman <c.atesman@gmx.net>
2016-03-01 13:43:22 +0100

    Release: 0.0.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.serve',
-    version='0.0.2',
+    version='0.0.3',
     description='Application server of The SCORE Framework',
     long_description=README,
     author='strg.at',
