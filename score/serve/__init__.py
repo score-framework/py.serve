73d0c99b3e088a1a2b2da5f0be97d10680fd8cb3
Necdet Can Ateşman <c.atesman@gmx.net>
2016-11-04 15:42:38 +0100

    Release: 0.1.7

--- score/serve/__init__.py
+++ score/serve/__init__.py
@@ -30,7 +30,7 @@ from .worker import (
     Worker, SocketServerWorker, SimpleWorker, AsyncioWorker, transitions)
 from .service import Service
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 __all__ = ('init', 'ConfiguredServeModule', 'Worker', 'SocketServerWorker',
            'SimpleWorker', 'AsyncioWorker', 'transitions', 'Service')
