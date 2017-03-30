693553db6e42ed2662d7f47a55da7fe7427f12a6
Necdet Can Ateşman <can@strg.at>
2017-03-30 10:58:13 +0200

    Fix: Prevent double-sending of service status
    The monitor was registering its state-change-listener twice: first in
    during connection_made(), and right afterwards in the set_instance()
    routine. Now only performing the initialization operations in
    set_instance().

--- score/serve/monitor.py
+++ score/serve/monitor.py
@@ -11,13 +11,12 @@ class ServiceMonitorProtocol(asyncio.Protocol):
 
     def connection_made(self, transport):
         self.transport = transport
-        self._conf.loop.create_task(self._send_service_states_async())
-        self.server.controller.on('state-change', self._state_change)
 
     def set_instance(self, server):
         assert self.server is None
         self.server = server
         self.server.controller.on('state-change', self._state_change)
+        self._conf.loop.create_task(self._send_service_states_async())
 
     def clear_instance(self, reloading):
         assert self.server is not None
@@ -28,13 +27,13 @@ class ServiceMonitorProtocol(asyncio.Protocol):
         self._conf._remove_monitor_connection(self)
 
     def _state_change(self, services):
+        if not services:
+            return
         services = OrderedDict((k, v.value) for k, v in services.items())
         self._send(json.dumps(services))
 
     @asyncio.coroutine
     def _send_service_states_async(self):
-        if self.server is None:
-            return
         services = yield from self.server.controller.service_states()
         self._state_change(services)
 
