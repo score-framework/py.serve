1e80af1fb9e12ddb3a0d16005cef45b0affdbd4d
Necdet Can Ateşman <can@strg.at>
2017-05-17 17:49:05 +0200

    Release: 0.1.13

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.serve',
-    version='0.1.12',
+    version='0.1.13',
     description='Application server of The SCORE Framework',
     long_description=README,
     author='strg.at',
